f44bc7fb2ac01d8a9cd746ca92ba0668691f0487
thatanjan <thatanjan@gmail.com>
2023-02-27 17:03:41 +0600

    Fix battery widget issue

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -45,6 +45,11 @@ widget_defaults = dict(
 extension_defaults = widget_defaults.copy()
 
 
+def is_laptop():
+    battery_path = '/sys/class/power_supply/BAT0'
+    return os.path.exists(battery_path)
+
+
 battery_widget = Battery(
     battery_name="BAT0",
     discharge_char='↓',
@@ -59,33 +64,42 @@ cpu_widget = CPU(
     foreground=['#FFFFFF', '#FFFF00'],
 )
 
+
+def widgets_init():
+    widget_list = [
+        widget.CurrentLayout(),
+        widget.GroupBox(),
+        widget.Prompt(),
+        widget.WindowName(),
+        widget.Chord(
+            chords_colors={
+                "launch": ("#ff0000", "#ffffff"),
+            },
+            name_transform=lambda name: name.upper(),
+        ),
+        # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
+        # widget.StatusNotifier(),
+        widget.Systray(),
+        widget.Volume(step=5),
+        cpu_widget,
+        widget.Net(
+            interface='enp42s0',
+            format='{down} ↓↑ {up}',
+        ),
+        widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
+        widget.QuickExit(),
+    ]
+
+    if is_laptop():
+        widget_list.insert(-4, battery_widget)
+
+    return widget_list
+
+
 screens = [
     Screen(
         bottom=bar.Bar(
-            [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        "launch": ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
-                # widget.StatusNotifier(),
-                widget.Systray(),
-                widget.Volume(step=5),
-                battery_widget,
-                cpu_widget,
-                widget.Net(
-                    interface='enp42s0',
-                    format='{down} ↓↑ {up}',
-                ),
-                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-                widget.QuickExit(),
-            ],
+            widgets_init(),
             24,
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
