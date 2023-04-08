83650195eb1a30e56561cd93f040ae2e5ad98381
thatanjan <thatanjan@gmail.com>
2023-04-08 19:42:53 +0600

    New monitor script

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
-#
-feh --bg-fill "https://proprikol.ru/wp-content/uploads/2020/04/kartinki-vulkany-27.jpg" &
+
+feh --bg-fill "https://images.pexels.com/photos/141876/pexels-photo-141876.jpeg?auto=compress&cs=tinysrgb&w=1600" &
 
 pulseaudio-equalizer enable &
 
 picom --config ~/.config/picom/picom.conf --experimental-backends &
 
 echo "" >>~/.config/picom/picom.conf &
+
+source ~/.dotfiles/my_scripts/monitor-screen.sh &
