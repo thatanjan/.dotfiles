e8535823454a3505a94999d2c231d0f494e4e469
Anjan Shomodder <thatanjan@gmail.com>
2023-07-04 19:15:25 +0600

    Don't display the vim mode prompt

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -132,25 +132,28 @@ function fish_greeting
     neofetch
 end
 
+# Display fish mode prompt
 function fish_mode_prompt
-  switch $fish_bind_mode
-    case default
-      set_color --bold red
-      echo 'N'
-    case insert
-      set_color --bold green
-      echo 'I'
-    case replace_one
-      set_color --bold green
-      echo 'R'
-    case visual
-      set_color --bold brmagenta
-      echo 'V'
-    case '*'
-      set_color --bold red
-      echo '?'
-  end
-  set_color normal
+  # switch $fish_bind_mode
+  #   case default
+  #     set_color --bold red
+  #     set_color -b green
+  #     echo 'N  '
+  #   case insert
+  #     set_color --bold green
+  #     echo 'I'
+  #   case replace_one
+  #     set_color --bold green
+  #     echo 'R'
+  #   case visual
+  #     set_color --bold brmagenta
+  #     echo 'V'
+  #   case '*'
+  #     set_color --bold red
+  #     echo '?'
+  # end
+  # set_color normal
+  # echo ''
 end
 
 # Use oh my posh
