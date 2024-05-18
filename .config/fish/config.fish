bcad7092c6d0f353f7efc2cae82598f15f8960d8
Anjan Shomodder <thatanjan@gmail.com>
2024-05-18 12:38:34 +0600

    Squashed commit of the following:
    commit a92f92214aa1819d8c8a00961584fbf5e90887bf
    Author: Anjan Shomodder <thatanjan@gmail.com>
    Date:   Sat May 18 12:31:42 2024 +0600
    
        Change git commit editor to neovim
    
    commit 0692c3370c3b94bd5a09e74645cd5fac0ec85d25
    Author: Anjan Shomodder <thatanjan@gmail.com>
    Date:   Sat May 18 12:30:47 2024 +0600
    
        add some changes
    
    commit 22f02d43b144c66f17b4ad2b66a02c2fed13d020
    Author: Anjan Shomodder <thatanjan@gmail.com>
    Date:   Wed May 15 10:21:55 2024 +0600
    
        rename nvim to neovide
    
    commit a32ca46008974dda285493738c82ba769e8e0d14
    Author: Anjan Shomodder <thatanjan@gmail.com>
    Date:   Wed May 15 10:20:22 2024 +0600
    
        Remove droppe alias and commented code
    
    commit e2386fcf280a97973f824bdd3699c5a5e16a919b
    Author: Anjan Shomodder <thatanjan@gmail.com>
    Date:   Wed May 15 10:09:24 2024 +0600
    
        Add rofi clipboard
    
    commit 916db12b310e9be535307ee768f751ed5329031a
    Author: Anjan Shomodder <thatanjan@gmail.com>
    Date:   Wed May 15 10:07:42 2024 +0600
    
        Add some font config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,29 +1,28 @@
 # Important
 alias cat=bat
 
-alias wcv='cd ~/.dotfiles/.config/hypr/ && nvim waybar/config'
-alias hcv='cd ~/.dotfiles/.config/hypr/ && nvim hyprland.conf'
+alias wcv='cd ~/.dotfiles/.config/hypr/ && neovide waybar/config'
+alias hcv='cd ~/.dotfiles/.config/hypr/ && neovide hyprland.conf'
 alias hc='cd ~/.dotfiles/.config/hypr/'
 
 alias red='redshift -l 23.810331:90.412521 -b'
-alias i3='nvim ~/.config/i3/config'
+alias i3='neovide ~/.config/i3/config'
 alias ri3='bat ~/.config/i3/config'
-alias ali='nvim ~/.config/fish/config.fish'
-alias rali='nvim ~/.config/fish/config.fish'
-alias kitcon='nvim ~/.config/kitty/kitty.conf'
+alias ali='neovide ~/.config/fish/config.fish'
+alias rali='neovide ~/.config/fish/config.fish'
+alias kitcon='neovide ~/.config/kitty/kitty.conf'
 
-alias nvimPer='cd ~/.dotfiles/.config/nvim/ && git checkout personal'
-alias nvimDro='cd ~/.dotfiles/.config/nvim/ && git checkout droppe'
+alias neovidePer='cd ~/.dotfiles/.config/neovide/ && git checkout personal'
 
 alias cdpor='cd ~/git_projects/portfolio'
 alias pors='cd ~/git_projects/portfolio && yarn dev'
-alias porv='cd ~/git_projects/portfolio && nvim src'
+alias porv='cd ~/git_projects/portfolio && neovide src'
 
 alias cdcfs='cd ~/git_projects/confession/server'
 alias cdcfc='cd ~/git_projects/confession/client'
-alias cf='cd ~/git_projects/confession/server && nvim src'
+alias cf='cd ~/git_projects/confession/server && neovide src'
 alias cfs='cd ~/git_projects/confession/server && yarn dev'
-alias cfc='cd ~/git_projects/confession/client&& nvim .'
+alias cfc='cd ~/git_projects/confession/client&& neovide .'
 alias cfcs='cd ~/git_projects/confession/client&& yarn dev'
 alias cfcbs='cd ~/git_projects/confession/server && yarn both-dev'
 
@@ -55,14 +54,14 @@ alias gm='git merge'
 alias gf='git fetch origin'
 
 # For configs
-alias v='nvim'
-alias nvcon='nvim ~/.config/nvim/init.lua'
-alias picoc='nvim ~/.config/picom/picom.conf'
-alias alac='nvim ~/.config/alacritty/alacritty.yml'
+alias v='neovide'
+alias nvcon='cd ~/.config/neovide/ && neovide init.lua'
+alias picoc='neovide ~/.config/picom/picom.conf'
+alias alac='neovide ~/.config/alacritty/alacritty.yml'
 alias liconv='v ~/.dotfiles'
 alias cdlico='cd ~/.dotfiles'
-alias qcon='nvim ~/.config/qtile/config.py'
-alias newv='cd  ~/.dotfiles/.config/nvim/ && nvim .'
+alias qcon='neovide ~/.config/qtile/config.py'
+alias newv='cd  ~/.dotfiles/.config/neovide/ && neovide .'
 
 
 # System
@@ -86,18 +85,6 @@ alias arco='sudo mount /dev/sda13 /mnt/arco'
 alias uarco='sudo umount /mnt/arco'
 alias cdarco='cd /mnt/arco/home/anjan'
 
-# for Droppe
-alias droppe='cd ~/git_projects/droppe-one/frontend/'
-alias droppev='cd ~/git_projects/droppe-one/frontend/ & nvim .'
-alias droppeu='cd ~/git_projects/droppe-one/ & docker-compose up'
-alias dropped='cd ~/git_projects/droppe-one/ & docker-compose down'
-
-alias droppeb='cd ~/git_projects/droppe-one/frontend-blog/'
-alias droppebv='cd ~/git_projects/droppe-one/frontend-blog/ & nvim .'
-alias droppebu='cd ~/git_projects/droppe-one/backend-blog/ & docker-compose up'
-alias droppebs='cd ~/git_projects/droppe-one/backend-blog/ & yarn develop'
-alias droppebd='cd ~/git_projects/droppe-one/backend-blog/ & docker-compose down'
-alias droppebcs='cd ~/git_projects/droppe-one/frontend-blog/ & yarn dev'
 
 alias pha='php artisan'
 
@@ -115,15 +102,10 @@ set PATH /var/lib/snapd/snap/bin $PATH
 set PATH /home/anjan/.cargo/bin $PATH
 set PATH /home/anjan/.config/composer/vendor/bin $PATH
 set PATH /home/anjan/.dotfiles/my_scripts/ $PATH
-# set PATH /.emacs.d/bin $PATH
-# set PATH /home/anjan/.emacs.d/bin $PATH
-
-# set PATH /home/anjan/scripts/ $PATH
 
 set -x QT_STYLE_OVERIDE GTK+
 set -x QT_QPA_PLATFORMTHEME qt5ct
 
-# set -U $budspencer_pwdstyle long
 
 set SUDO_EDITOR 'nvim'
 set GIT_EDITOR 'nvim'
@@ -136,31 +118,8 @@ function fish_greeting
     # neofetch
 end
 
-# Display fish mode prompt
-function fish_mode_prompt
-  # switch $fish_bind_mode
-  #   case default
-  #     set_color --bold red
-  #     set_color -b green
-  #     echo 'N  '
-  #   case insert
-  #     set_color --bold green
-  #     echo 'I'
-  #   case replace_one
-  #     set_color --bold green
-  #     echo 'R'
-  #   case visual
-  #     set_color --bold brmagenta
-  #     echo 'V'
-  #   case '*'
-  #     set_color --bold red
-  #     echo '?'
-  # end
-  # set_color normal
-  # echo ''
-end
 
-# Use oh my posh
-# oh-my-posh init fish | source
-# oh-my-posh init fish --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/easy-term.omp.json' | source
-oh-my-posh init fish --config '~/.mytheme.omp.json' | source
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
